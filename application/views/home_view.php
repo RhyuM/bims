561ff5242fc5509069f7971c0f8ced112f56393d
jepeu <mmacki1313@gmail.com>
2021-06-21 14:35:50 +0700

    edited home page

--- application/views/home_view.php
+++ application/views/home_view.php
@@ -15,21 +15,21 @@
                   <h6>Our Mission</h6>
                   <p>Procurement Service is the trusted and preferred procurement partner of the public sector in the Philippines providing smart procurement solutions.
 
-</p>  <a href="http://localhost/bims/contact" class="filled-button">contact us</a>
+
                 </div>
             </div>
           </div>
           <!-- // Item -->
-          <!-- Item -->
+          <!-- Item 
           <div class="item item-2">
             <div class="img-fill">
-                <div class="text-content">
+                <div class="text-content sliderimg">
                   
-                  <h6>Manticao Bidding Staff</h6>
-                  <p>With honor to our onganization we shall deliver bidding with no doubt</p>  <a href="services.html" class="filled-button">our services</a>
-                </div>
+                     <img class="sliderimg" style="text-align:left" src=custom/assets/images/pic/img2.png" alt="img"> 
+                    </div>
             </div>
           </div>
+-->
           <!-- // Item -->
           <!-- Item -->
           <div class="item item-3">
@@ -38,7 +38,7 @@
                  
                   <h6>Our Vission</h6>
                   <p> Procure quality government requirements efficiently and economically from reputable sources, provide excellent customer service responsive to the needs of its stakeholders, foster integrity and a fair, transparent, and competitive environment in procurement, develop its human capital as the country’s leading procurement and supply chain professionals, ensure stable, progressive, and sustainable operations and Introduce and sustain smart and innovative products and procurement solutions.</p>     
-                   <a href="http://localhost/bims/about" class="filled-button">learn more</a>
+                  
                 </div>
             </div>
           </div>
@@ -72,31 +72,31 @@
           </div>
           <div class="col-md-4">
             <div class="service-item">
-              <img src="<?php echo base_url()."assets/";?>custom/assets/images/staff/pic1.jpg" style="heigth" alt="">
+              <img src="<?php echo base_url()."assets/";?>custom/assets/images/staff/staff3.jpg" style="heigth" alt="">
               <div class="down-content">
                 <h4  style="text-align:center">BAC Secretary<br> ( BACSEC ) </h4>
-                <p>Sed tincidunt dictum lobortis. Aenean tempus diam vel augue luctus dignissim. Nunc ornare leo tortor.</p>
-                <a href="" class="filled-button">Read More</a>
+                <p style="text-align:justify;text-justify:inter-word;text-indent:51px">The BAC Secretariat shall ensure that all procurements undertaken by the Procuring Entity are properly documented, to provide an audit trail of the procurement process.</p>
+                
               </div>
             </div>
           </div>
           <div class="col-md-4">
             <div class="service-item">
-              <img src="<?php echo base_url()."assets/";?>custom/assets/images/staff/pic2.jpg" alt="">
+              <img  src="<?php echo base_url()."assets/";?>custom/assets/images/staff/staff3.jpg" alt="">
               <div class="down-ycontent">
-                <h4 style="text-align:center;padding-top:28px">Bids And Awards Comittee <br> ( BAC )  </h4>
-                <p>Sed tincidunt dictum lobortis. Aenean tempus diam vel augue luctus dignissim. Nunc ornare leo tortor.</p>
-                <a href="" class="filled-button">Read More</a>
+                <h4 style="text-align:center;padding-top:28px">Bids And Awards Comittee ( BAC )  </h4>
+                <p style="text-align:justify;text-justify:inter-word;text-indent:51px">The BAC shall have the following functions: advertise and/or post the invitation to bid, conduct pre-procurement and pre-bid conferences, determine the eligibility of prospective bidders, receive bids, conduct the evaluation of bids, undertake post-qualification proceedings, recommend award of contracts to the Head </p><br>
+             
               </div>
             </div>
           </div>
           <div class="col-md-4">
             <div class="service-item">
-              <img src="<?php echo base_url()."assets/";?>custom/assets/images/service_03.jpg" alt="">
+              <img src="<?php echo base_url()."assets/";?>custom/assets/images/staff/staff3.jpg" alt="">
               <div class="down-content">
-                <h4 style="text-align:center">Techncial Working Group <br> ( TWG )</h4>
-                <p>Sed tincidunt dictum lobortis. Aenean tempus diam vel augue luctus dignissim. Nunc ornare leo tortor.</p>
-                <a href="" class="filled-button">Read More</a>
+                <h4 style="text-align:center">Techncial Working Group ( TWG )</h4>
+                <p style="text-align:justify;text-justify:inter-word;text-indent:51px">Assist the BAC in the preparation of the bidding documents, ensuring that the same properly reflects the requirements of the Procuring Entity and that these conform to the standards set forth by R.A. 9184, its IRR-A and the PBDs prescribed by the GPPB</p>
+               
               </div>
             </div>
           </div>
@@ -112,19 +112,22 @@
               <span>Lorem ipsum dolor sit amet</span>
               <h2>Our solutions for your <em>business growth</em></h2>
               <p>Pellentesque ultrices at turpis in vestibulum. Aenean pretium elit nec congue elementum. Nulla luctus laoreet porta. Maecenas at nisi tempus, porta metus vitae, faucibus augue. 
-              <br><br>Fusce et venenatis ex. Quisque varius, velit quis dictum sagittis, odio velit molestie nunc, ut posuere ante tortor ut neque.</p>
-              <a href="" class="filled-button">Read More</a>
+              Fusce et venenatis ex. Quisque varius, velit quis dictum sagittis, odio velit molestie nunc, ut posuere ante tortor ut neque.</p>
+           
+             <!--  <a href="" class="filled-button">Read More</a> -->
             </div>
           </div>
-          <!--
+      
           <div class="col-md-6 align-self-center">
             <div class="row">
               <div class="col-md-6">
-                <div class="count-area-content">
-                  <div class="count-digit">945</div>
-                  <div class="count-title">Work Hours</div>
+                <div class="count-area-content sliderimg">
+      
+                <img style="padding-right:120px;width:567px;"src="<?php echo base_url()."assets/";?>custom/assets/images/process/pro1.jpg" alt="">
                 </div>
               </div>
+</div>
+              <!---
               <div class="col-md-6">
                 <div class="count-area-content">
                   <div class="count-digit">1280</div>
@@ -141,7 +144,7 @@
                 <div class="count-area-content">
                   <div class="count-digit">26</div>
                   <div class="count-title">Awards Won</div>
--->
+            -->
                 </div>
               </div>
             </div>
@@ -158,15 +161,15 @@
               <div class="row">
                 <div class="col-md-6">
                   <div class="left-image">
-                    <img src="<?php echo base_url()."assets/";?>custom/assets/images/more-info.jpg" alt="">
-                  </div>
+                  <img class="sliderimg" style="text-align:left" src="<?php echo base_url()."assets/";?>custom/assets/images/pic/img2.png" alt="img"> 
+                    </div>
                 </div>
                 <div class="col-md-6 align-self-center">
                   <div class="right-content">
                     <span>Who we are</span>
                     <h2>Get to know about <em>Manticao</em></h2>
-                    <p>Curabitur pulvinar sem a leo tempus facilisis. Sed non sagittis neque. Nulla conse quat tellus nibh, id molestie felis sagittis ut. Nam ullamcorper tempus ipsum in cursus<br><br>Praes end at dictum metus. Morbi id hendrerit lectus, nec dapibus ex. Etiam ipsum quam, luctus eu egestas eget, tincidunt</p>
-                    <a href="#" class="filled-button">Read More</a>
+                    <p> Manticao, officially the Municipality of Manticao, is a 4th class municipality in the province of Misamis Oriental, Philippines. According to the 2015 census, it has a population of 28,422 people. Manticao River was known as Naawan River until 1957, when it was renamed.  </p> 
+                         <a href="#" class="filled-button">Read More</a>
                   </div>
                 </div>
               </div>
